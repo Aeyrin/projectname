3eab331790d3e91ea7275b3dc2e6dfe42fc22520
Aeyrin <Aeyrinn@gmail.com>
2022-02-06 00:36:07 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ struct GreetTimer (Timer);
 fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
     if timer.0.tick(time.delta()).just_finished()   {
         for name in query.iter ()   {
-            println! ("hello there {}!", name.0);
+            println! ("aaa eeeeee aaaaa e {}!", name.0);
         }
     }    
 }
