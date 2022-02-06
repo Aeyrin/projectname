9b3b39ba3382c683ce5b1fda35d532643ddd4ca2
Aeyrin <Aeyrinn@gmail.com>
2022-02-06 13:45:22 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,7 @@ use bevy::prelude::*;
 use bevy_physimple::prelude::*;
 
 #[derive(Component)]
-struct Person;
-
-#[derive(Component)]
 struct Player;
-
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app
-            .insert_resource(GreetTimer(Timer::from_seconds(99.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-        // add things to your app here
-    }
-}
 
 fn move_player (mut query: Query<(&Player, &mut Transform)>,  input: Res<Input<KeyCode>>) {
     let (_player, mut transform) = query.single_mut();
@@ -43,22 +25,6 @@ fn move_player (mut query: Query<(&Player, &mut Transform)>,  input: Res<Input<K
     }*/
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Erin :D".to_string()));
-    commands.spawn().insert(Person).insert(Name("Kairo :(".to_string()));
-    commands.spawn().insert(Person).insert(Name("hello".to_string()));
-}
-
-struct GreetTimer (Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished()   {
-        for name in query.iter ()   {
-            println! ("aaa eeeeee aaaaa e {}!", name.0);
-        }
-    }    
-}
-
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(SpriteBundle {
@@ -71,7 +37,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(Physics2dPlugin)
-        .add_plugin(HelloPlugin)
         .add_startup_system(setup)
         .add_system(move_player)
         .run();
