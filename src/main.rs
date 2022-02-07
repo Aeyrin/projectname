7ca34830946e366516ea8f8c7cdd8dab6b25e708
ygypt <70084790+ygypt@users.noreply.github.com>
2022-02-07 00:22:15 +0000

    made physics sorta

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ const PLAYER_SPEED: f32 = 10.0;
 
 pub struct PlayerStats {
     pub speed: f32,
+    pub terminal: f32,
+    pub friction: f32,
 }
 impl Default for PlayerStats {
     fn default() -> Self {
         Self{
-            speed: 1000.0,
+            speed: 10000.0,
+            terminal: 1000.0,
+            friction: 1.4,
         }
     }
 }
@@ -60,6 +64,7 @@ fn gravity (
     mut query: Query<&mut Vel>,
     time: Res<Time>,
     gravity: Res<Gravity>,
+    stats: Res<PlayerStats>,
 ) {
     // Vel is a component that's part of the KinematicBundle, its the
     // velocity of entity. this equation will constantly apply
@@ -67,15 +72,31 @@ fn gravity (
     // its consistent regardless of framerate
     for mut vel in query.iter_mut() {
         vel.0 += time.delta_seconds() * gravity.0;
+        console(format!("{}", vel.0));
+        // totally unrelated, create friction and apply terminal velocity
+        vel.0.y = vel.0.y.clamp(-stats.terminal, stats.terminal,);
+        vel.0.x = vel.0.x.clamp(-stats.terminal, stats.terminal,);
+
+        if vel.0.x != 0. {
+            vel.0.x = vel.0.x / stats.friction;
+        }
+        /*if vel.0.y != 0. {
+            vel.0.y = vel.0.y / stats.friction;
+        }*/
+        /*if vel.0 > 1000 {
+            vel.0 /= time.delta_seconds() * 2.0;
+        }*/
+        
     }
 }
+
 
 // STARTUP SYSTEMS
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     
     // gravity
-    commands.insert_resource(Gravity(Vec2::new(0.0,-540.0)));
+    commands.insert_resource(Gravity(Vec2::new(0.0,-5000.0)));
 
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -103,7 +124,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         })
         .insert_bundle(StaticBundle {
             marker: StaticBody,
-            shape: CollisionShape::Square(Square::size(Vec2::new(64.0, 64.0))),
+            shape: CollisionShape::Square(Square::size(Vec2::new(2525.0, 64.0))),
             coll_layer: CollisionLayer::default(),
         })
     ;
